9aced5060ca42c1a08f5a1ba403b9b8a4bae689b
Jon <bit172@gmail.com>
2022-09-22 18:48:20 -0400

    Initial commit

---
+++ components/banner.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import styles from './banner.module.css';
+
+type BannerProps = {
+  buttonText: string;
+  handleOnClick: () => void;
+};
+
+const Banner: React.FC<BannerProps> = ({ buttonText, handleOnClick }) => {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>
+        <span className={styles.title1}>Coffee</span>
+        <span className={styles.title2}>Connoisseur</span>
+      </h1>
+      <p className={styles.subTitle}>Discover your local coffee shops!</p>
+      <button className={styles.button} onClick={handleOnClick}>
+        {buttonText}
+      </button>
+    </div>
+  );
+};
+
+export default Banner;
